9d451fdd72192d0340e1ecc1561faef0266b8d9b
Blair Erickson <blair@jamwix.com>
2017-04-12 14:00:38 -0400

    updated error handling for null IDs

--- bamazon.js
+++ bamazon.js
@@ -29,46 +29,57 @@ function bamselect()
     inquirer.prompt([
         {
             name: "actionchoice",
-            message: "Welcome Bamazon! \n What is the ID of the item you want to buy? \n ID:",
+            message: "Welcome Bamazon! \n What is the ID of the item you want to buy? \n Or enter 0 to quit. \n ID:",
         }, ])
         .then(function (answers) {
-        console.log("ID# selected: " + answers.actionchoice);
-        id = answers.actionchoice;
-        buyitem();
+            if (answers.actionchoice == 0)
+            {
+                console.log("\n goodbye! \n");
+                connection.end();
+            }
+            else {
+                console.log("ID# selected: " + answers.actionchoice);
+                id = answers.actionchoice;
+                buyitem();
+            }
     });
 };
 
 function buyitem()
 {
     connection.query('SELECT * FROM items WHERE item_id =' + id, function (error, results, fields) {
-        if (error) throw error;
-        console.log("ID #" + results[0].item_id + "  Item name: " + results[0].product_name + "  price: $" + results[0].price + "  type: " + results[0].department_name + "  stock: " + results[0].stock_quantity);
-        inventory = results[0].stock_quantity;
-        item_price = results[0].price;
-        inquirer.prompt([
-            {
-                name: "actionchoice",
-                message: "How many do you want to buy? \n Amount:",
-            }, ])
-            .then(function (answers) {
-                inventory = inventory - answers.actionchoice;
-                if (inventory < 0)
+        if (error) {
+            console.log("Not a valid ID. \n")
+        listings();
+        }
+        else {
+            console.log("ID #" + results[0].item_id + "  Item name: " + results[0].product_name + "  price: $" + results[0].price + "  type: " + results[0].department_name + "  stock: " + results[0].stock_quantity);
+            inventory = results[0].stock_quantity;
+            item_price = results[0].price;
+            inquirer.prompt([
                 {
-                 console.log("Not enough left in stock!");
-                 listings();
-                }
-                else {
-                    item_price = item_price * answers.actionchoice;
-                    console.log("Total cost $" + item_price);
-                    connection.query('UPDATE items SET stock_quantity=' + inventory + ' WHERE item_id = ' + id, function (error, results, fields) {
-                        if (error) throw error;
-                        console.log(inventory + " left in stock.");
+                    name: "actionchoice",
+                    message: "How many do you want to buy? \n Amount:",
+                },])
+                .then(function (answers) {
+                    inventory = inventory - answers.actionchoice;
+                    if (inventory < 0) {
+                        console.log("Not enough left in stock!");
                         listings();
-                    });
-                };
-            });
+                    }
+                    else {
+                        item_price = item_price * answers.actionchoice;
+                        console.log("Total cost $" + item_price);
+                        connection.query('UPDATE items SET stock_quantity=' + inventory + ' WHERE item_id = ' + id, function (error, results, fields) {
+                            if (error) throw error;
+                            console.log(inventory + " left in stock.");
+                            listings();
+                        });
+                    }
+                    ;
+                });
 
-
+        }
 
     });
 };
